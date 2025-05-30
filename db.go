491d4a67df7fb728384077e144b864870f6b2aec
Fohlii <fohli54@gmail.com>
2025-05-30 18:08:05 +0200

    now seriallorparallel will also be written into the database

--- db.go
+++ db.go
@@ -76,6 +76,7 @@ type Operation struct {
 	QGateRelevant      *string                `gorm:"default:null"`
 	SerialOrParallel   *string                `gorm:"default:null"`
 	Template           *string                `gorm:"default:null"`
+	SerialOrParallel   *string                `gorm:"default:null"`
 	DecisionClass      *string                `gorm:"default:null"`
 	SavingClass        *string                `gorm:"default:null"`
 	VerificationClass  *string                `gorm:"default:null"`
