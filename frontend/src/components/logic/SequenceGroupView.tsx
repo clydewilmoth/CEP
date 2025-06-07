38cb7805cf57e4ebb38f6bbee23cd599a84a93f9
Daweenci <we3bk1ng@gmail.com>
2025-06-07 15:52:00 +0200

    refactored useEffect groups logic into existing useQuery entitiesSequenceGroup, got rid of redundant code

--- frontend/src/components/logic/SequenceGroupView.tsx
+++ frontend/src/components/logic/SequenceGroupView.tsx
@@ -1,16 +1,15 @@
 import { useQueryClient, useMutation, useQuery } from "@tanstack/react-query";
 import {
-    GetAllEntities,
-    GetOperationsByStation,
-    CreateEntity,
+  GetAllEntities,
+  GetOperationsByStation,
+  CreateEntity,
 } from "../../../wailsjs/go/main/Core";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { toast } from "sonner";
-import React from "react";
 import { useTranslation } from "react-i18next";
 import { useParams } from "wouter";
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { Card, CardTitle } from "@/components/ui/card";
 import { DeleteEntityDialog } from "./EntityCollection";
 import {
@@ -22,111 +21,114 @@ import { Ellipsis } from "lucide-react";
 import { ScrollArea } from "../ui/scroll-area";
 import { Reorder } from "framer-motion";
 
-
 export function SequenceGroupView({
-    entityType,
-    parentId,
+  entityType,
+  parentId,
 }: {
-    entityType: string;
-    parentId: string;
+  entityType: string;
+  parentId: string;
 }) {
-    const { suuid } = useParams<{ luuid: string; suuid: string; tuuid: string; }>();
-    const { data: entitiesOp } = useQuery({
-        queryKey: ["entitiesOp", entityType, parentId],
-        queryFn: async () => await GetOperationsByStation(suuid),
-    });
-    const { data: entitiesSequenceGroup } = useQuery({
-        queryKey: ["entities", entityType, parentId],
-        queryFn: async () => await GetAllEntities(entityType, String(parentId)),
-    });
+  const { suuid } = useParams<{ luuid: string; suuid: string; tuuid: string }>();
+  const { t } = useTranslation();
 
-    type Operation = {
-      ID: string;
-      Name: string;
-      SequenceGroup: string;
-      Sequence: number;
-    };
-    type Group = {
-      ID: string;
-      Name: string;
-      Index: string;
-      Operations: Operation[];
-    };
-    const [groups, setGroups] = useState<Group[]>([]);
-    const [inputValue, setInputValue] = useState("");
-    const { t } = useTranslation();
-
-  useEffect(() => {
-  if (!entitiesSequenceGroup) return;
-
-  const fetchGroupsWithOperations = async () => {
-    const newGroups: any[] = await Promise.all(
-      entitiesSequenceGroup.map(async (entity: any) => {
-        const operationFull: any[] = await GetAllEntities("operation", entity.ID);
-        const operations: Operation[] = (operationFull ?? []).map((op: any) => ({
-          ID: op.ID,
-          Name: op.Name,
-          SequenceGroup: op.SequenceGroup,
-          Sequence: op.Sequence,
-        })).sort((a, b) => a.Sequence - b.Sequence);
-        return {
-          ID: entity.ID,
-          Index: entity.Index,
-          Operations: operations,
-        };
-      })
-    );
-    newGroups.sort((a, b) => a.Index - b.Index);
-    setGroups(newGroups);
+  type Operation = {
+    ID: string;
+    Name: string;
+    SequenceGroup: string;
+    Sequence: number;
   };
 
-  fetchGroupsWithOperations();
-}, [entitiesSequenceGroup]); //noch nicht so sinnvoller hook? vllt []
-    
-    return (
-        <div className="grid grid-cols-2 px-5">
-          
-          <ScrollArea className="h-[87.5vh]">
-            <div className="flex flex-col gap-3 p-8">
-              {entitiesOp?.map((entity, index) => {
-                return (
-                <Card key={index}
-                      className="w-36 h-fit flex relative justify-center items-center hover:cursor-pointer hover:translate-y-1 transition-all">
-                  <div className="fon-bold text-sm text-center">
-                    {entity.Name || t("unnamed_entity")}
-                  </div>
-                </Card>
-                );
-              })}
-            </div>
-          </ScrollArea>
-          
-          <ScrollArea className="h-[87.5vh]">
-            <div className="flex flex-col gap-3 p-8">
-              <Reorder.Group values={groups} onReorder={setGroups} className="flex flex-col gap-3">
-              {entitiesSequenceGroup?.map((entity, index) => {
-                return (
-                  <SequenceGroupCard
-                    key={index}
-                    entityType={entityType}
-                    entityId={entity.ID}
-                    entityName={entity.Name || t("unnamed_group")}
-                  />
-                );
-              })}
-              </Reorder.Group>
-              <Input value={inputValue} onChange={e => setInputValue(e.target.value)} />
-              <CreateSequenceGroupCard
-                  name={t("Create Sequencegroup")}
-                  entityType={entityType}
-                  parentId={parentId}
-                  sequenceGroupName={inputValue}
-              />
-          </div>
-          </ScrollArea>
-          
+  type Group = {
+    ID: string;
+    Name: string;
+    Index: string;
+    Operations: Operation[];
+  };
+
+  const { data: entitiesOp } = useQuery({
+    queryKey: ["entitiesOp", entityType, parentId],
+    queryFn: async () => await GetOperationsByStation(suuid),
+  });
+
+  const { data: entitiesSequenceGroup } = useQuery({
+    queryKey: ["entitiesSequenceGroup", entityType, parentId],
+    queryFn: async (): Promise<Group[]> => {
+      const groups = await GetAllEntities(entityType, String(parentId));
+      const groupsWithOperations: Group[] = await Promise.all(
+        (groups ?? []).map(async (group: any) => {
+          const rawOps = await GetAllEntities("operation", group.ID);
+          const operations: Operation[] = (rawOps ?? []).map((op: any) => ({
+            ID: op.ID,
+            Name: op.Name,
+            SequenceGroup: op.SequenceGroup,
+            Sequence: op.Sequence,
+          })).sort((a, b) => a.Sequence - b.Sequence);
+
+          return {
+            ID: group.ID,
+            Name: group.Name ?? "Unnamed",
+            Index: group.Index,
+            Operations: operations,
+          };
+        })
+      );
+      return groupsWithOperations.sort((a, b) => parseInt(a.Index) - parseInt(b.Index));
+    },
+  });
+
+  const [inputValue, setInputValue] = useState("");
+
+  return (
+    <div className="grid grid-cols-2 px-5">
+      <ScrollArea className="h-[87.5vh]">
+        <div className="flex flex-col gap-3 p-8">
+          {entitiesOp?.map((entity, index) => (
+            <Card
+              key={index}
+              className="w-36 h-fit flex relative justify-center items-center hover:cursor-pointer hover:translate-y-1 transition-all"
+            >
+              <div className="font-bold text-sm text-center">
+                {entity.Name || t("unnamed_entity")}
+              </div>
+            </Card>
+          ))}
         </div>
-    );
+      </ScrollArea>
+
+      <ScrollArea className="h-[87.5vh]">
+        <div className="flex flex-col gap-3 p-8">
+          <Reorder.Group
+            values={entitiesSequenceGroup ?? []}
+            onReorder={() => {}}
+            className="flex flex-col gap-3"
+          >
+            {entitiesSequenceGroup?.map((group, index) => (
+              <Reorder.Item value={group} key={index}>
+                <SequenceGroupCard
+                  key={index}
+                  entityType={entityType}
+                  entityId={group.ID}
+                  entityName={group.Name || t("unnamed_group")}
+                />
+              </Reorder.Item>
+            ))}
+          </Reorder.Group>
+
+          <Input
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+
+          <CreateSequenceGroupCard
+            name={t("Create Sequencegroup")}
+            entityType={entityType}
+            parentId={parentId}
+            sequenceGroupName={inputValue}
+          />
+        </div>
+      </ScrollArea>
+    </div>
+  );
 }
 
 function CreateSequenceGroupCard({
@@ -197,26 +199,24 @@ function SequenceGroupCard({
   return (
     <Card className="w-36 h-fit flex relative justify-center items-center hover:cursor-pointer hover:translate-y-1 transition-all">
       <div>
-      <DropdownMenu key={key}>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" size="icon">
-            <Ellipsis />
-          </Button>
-        </DropdownMenuTrigger>
+        <DropdownMenu key={key}>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" size="icon">
+              <Ellipsis />
+            </Button>
+          </DropdownMenuTrigger>
 
-        <DropdownMenuContent onClick={(e) => e.stopPropagation()}>
-          <DeleteEntityDialog
-            entityType={entityType}
-            entityId={entityId}
-            onClose={() => setKey((k) => k + 1)}
-          />
-        </DropdownMenuContent>
-      </DropdownMenu>
+          <DropdownMenuContent onClick={(e) => e.stopPropagation()}>
+            <DeleteEntityDialog
+              entityType={entityType}
+              entityId={entityId}
+              onClose={() => setKey((k) => k + 1)}
+            />
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
       <div>
-        <CardTitle>
-          {entityName || t("unnamed_group")}
-        </CardTitle>
+        <CardTitle>{entityName || t("unnamed_group")}</CardTitle>
       </div>
     </Card>
   );
